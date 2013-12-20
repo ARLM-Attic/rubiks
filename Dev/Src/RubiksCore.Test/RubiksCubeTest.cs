b1c976728a7bb26c6570162e3e746bf23037296f
SND\danielsandberg_cp <SND\danielsandberg_cp@1eac2b61-d0d7-4846-87ee-9410fe51cbb3>
2013-12-20 21:06:24 +0000

    Added rubikscore class library and associated test project
    git-svn-id: https://rubiks.svn.codeplex.com/svn@30908 1eac2b61-d0d7-4846-87ee-9410fe51cbb3

---
+++ Dev/Src/RubiksCore.Test/RubiksCubeTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RubiksCore.Test
+{
+    [TestClass]
+    public class RubiksCubeTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
