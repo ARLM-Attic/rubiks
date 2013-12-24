14766681f6578d5264709e2979567253de7adebe
SND\danielsandberg_cp <SND\danielsandberg_cp@1eac2b61-d0d7-4846-87ee-9410fe51cbb3>
2013-12-24 23:35:08 +0000

    Finished tests for CubefACE
    git-svn-id: https://rubiks.svn.codeplex.com/svn@30996 1eac2b61-d0d7-4846-87ee-9410fe51cbb3

--- Dev/Src/RubiksCore.Test/CubeFaceTest.cs
+++ Dev/Src/RubiksCore.Test/CubeFaceTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 
@@ -136,37 +137,256 @@ namespace RubiksCore.Test
         [TestMethod]
         public void InitializeCubeFace_WhenCubeSizeIsThreeAndDirectionIsBack_ThenCubiePositionsAreAccurate()
         {
-            Assert.Inconclusive();
+            //setup
+            List<Position> expectedPositions = new List<Position>();
+
+            Position position000 = new Position()
+            {
+                X = 0,
+                Y = 0,
+                Z = 0
+            };
+            expectedPositions.Add(position000);
+
+            Position position100 = new Position()
+            {
+                X = 1,
+                Y = 0,
+                Z = 0
+            };
+            expectedPositions.Add(position100);
+
+            Position position200 = new Position()
+            {
+                X = 2,
+                Y = 0,
+                Z = 0
+            };
+            expectedPositions.Add(position200);
+
+            Position position001 = new Position()
+            {
+                X = 0,
+                Y = 0,
+                Z = 1
+            };
+            expectedPositions.Add(position001);
+
+            Position position101 = new Position()
+            {
+                X = 1,
+                Y = 0,
+                Z = 1
+            };
+            expectedPositions.Add(position101);
+
+            Position position201 = new Position()
+            {
+                X = 2,
+                Y = 0,
+                Z = 1
+            };
+            expectedPositions.Add(position201);
+
+            Position position002 = new Position()
+            {
+                X = 0,
+                Y = 0,
+                Z = 2
+            };
+            expectedPositions.Add(position002);
+
+            Position position102 = new Position()
+            {
+                X = 1,
+                Y = 0,
+                Z = 2
+            };
+            expectedPositions.Add(position102);
+
+            Position position202 = new Position()
+            {
+                X = 2,
+                Y = 0,
+                Z = 2
+            };
+            expectedPositions.Add(position202);
+
+            //exercise
+            CubeFace face = new CubeFace(RubiksDirection.Back, 3);
+
+            //verification
+            Assert.IsTrue(face.CubiePositions.SequenceEqual(expectedPositions));
         }
 
         [TestMethod]
         public void InitializeCubeFace_WhenCubeSizeIsThreeAndDirectionIsRight_ThenCubiePositionsAreAccurate()
         {
-            Assert.Inconclusive();
+            //setup
+            List<Position> expectedPositions = new List<Position>();
+
+            Position position200 = new Position()
+            {
+                X = 2,
+                Y = 0,
+                Z = 0
+            };
+            expectedPositions.Add(position200);
+
+            Position position210 = new Position()
+            {
+                X = 2,
+                Y = 1,
+                Z = 0
+            };
+            expectedPositions.Add(position210);
+
+            Position position220 = new Position()
+            {
+                X = 2,
+                Y = 2,
+                Z = 0
+            };
+            expectedPositions.Add(position220);
+
+            Position position201 = new Position()
+            {
+                X = 2,
+                Y = 0,
+                Z = 1
+            };
+            expectedPositions.Add(position201);
+
+            Position position211 = new Position()
+            {
+                X = 2,
+                Y = 1,
+                Z = 1
+            };
+            expectedPositions.Add(position211);
+
+            Position position221 = new Position()
+            {
+                X = 2,
+                Y = 2,
+                Z = 1
+            };
+            expectedPositions.Add(position221);
+
+            Position position202 = new Position()
+            {
+                X = 2,
+                Y = 0,
+                Z = 2
+            };
+            expectedPositions.Add(position202);
+
+            Position position212 = new Position()
+            {
+                X = 2,
+                Y = 1,
+                Z = 2
+            };
+            expectedPositions.Add(position212);
+
+            Position position222 = new Position()
+            {
+                X = 2,
+                Y = 2,
+                Z = 2
+            };
+            expectedPositions.Add(position222);
+
+            //exercise
+            CubeFace face = new CubeFace(RubiksDirection.Back, 3);
+
+            //verification
+            Assert.IsTrue(face.CubiePositions.SequenceEqual(expectedPositions));
         }
 
         [TestMethod]
-        public void InitializeCubeFace_WhenCubeSizeIsThreeAndDirectionIsTop_ThenCubiePositionsAreAccurate()
+        public void InitializeCubeFace_WhenCubeSizeIsThreeAndDirectionIsUp_ThenCubiePositionsAreAccurate()
         {
-            Assert.Inconclusive();
-        }
+            //setup
+            List<Position> expectedPositions = new List<Position>();
 
-        [TestMethod]
-        public void InitializeCubeFace_WhenCubeSizeIsFourAndDirectionIsBack_ThenCubiePositionsAreAccurate()
-        {
-            Assert.Inconclusive();
-        }
+            Position position002 = new Position()
+            {
+                X = 0,
+                Y = 0,
+                Z = 2
+            };
+            expectedPositions.Add(position002);
 
-        [TestMethod]
-        public void InitializeCubeFace_WhenCubeSizeIsFourAndDirectionIsRight_ThenCubiePositionsAreAccurate()
-        {
-            Assert.Inconclusive();
-        }
+            Position position012 = new Position()
+            {
+                X = 0,
+                Y = 1,
+                Z = 2
+            };
+            expectedPositions.Add(position012);
 
-        [TestMethod]
-        public void InitializeCubeFace_WhenCubeSizeIsFourAndDirectionIsTop_ThenCubiePositionsAreAccurate()
-        {
-            Assert.Inconclusive();
+            Position position022 = new Position()
+            {
+                X = 0,
+                Y = 2,
+                Z = 2
+            };
+            expectedPositions.Add(position022);
+
+            Position position102 = new Position()
+            {
+                X = 1,
+                Y = 0,
+                Z = 2
+            };
+            expectedPositions.Add(position102);
+
+            Position position112 = new Position()
+            {
+                X = 1,
+                Y = 1,
+                Z = 2
+            };
+            expectedPositions.Add(position112);
+
+            Position position122 = new Position()
+            {
+                X = 1,
+                Y = 2,
+                Z = 2
+            };
+            expectedPositions.Add(position122);
+
+            Position position202 = new Position()
+            {
+                X = 2,
+                Y = 0,
+                Z = 2
+            };
+            expectedPositions.Add(position202);
+
+            Position position212 = new Position()
+            {
+                X = 2,
+                Y = 1,
+                Z = 2
+            };
+            expectedPositions.Add(position212);
+
+            Position position222 = new Position()
+            {
+                X = 2,
+                Y = 2,
+                Z = 2
+            };
+            expectedPositions.Add(position222);
+
+            //exercise
+            CubeFace face = new CubeFace(RubiksDirection.Up, 3);
+
+            //verification
+            Assert.IsTrue(face.CubiePositions.SequenceEqual(expectedPositions));
         }
     }
 }
