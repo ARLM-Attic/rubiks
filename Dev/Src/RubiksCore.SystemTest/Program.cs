02c84fa30fb0beb37e9b5d63d90477245661447c
SND\danielsandberg_cp <SND\danielsandberg_cp@1eac2b61-d0d7-4846-87ee-9410fe51cbb3>
2014-01-01 02:01:59 +0000

    Created a system test project for RubiksCore
    git-svn-id: https://rubiks.svn.codeplex.com/svn@31314 1eac2b61-d0d7-4846-87ee-9410fe51cbb3

---
+++ Dev/Src/RubiksCore.SystemTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubiksCore.SystemTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RubiksCube cube = new RubiksCube(null);
+
+            Console.WriteLine(cube);
+
+
+            cube.TurnBack();
+            Console.WriteLine(cube);
+            cube.TurnDown();
+            Console.WriteLine(cube);
+            cube.TurnFront();
+            Console.WriteLine(cube);
+            cube.TurnLeft();
+            Console.WriteLine(cube);
+            cube.TurnBack();
+            Console.WriteLine(cube);
+            Console.ReadLine();
+            
+        }
+    }
+}
