64b9b35adcc44d49a34a0d51a653a5c218f97004
SND\danielsandberg_cp <SND\danielsandberg_cp@1eac2b61-d0d7-4846-87ee-9410fe51cbb3>
2014-02-16 21:38:35 +0000

    Created an initial version of shell
    git-svn-id: https://rubiks.svn.codeplex.com/svn@32071 1eac2b61-d0d7-4846-87ee-9410fe51cbb3

---
+++ Dev/Src/RubiksShell/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.ServiceLocation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RubiksShell
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override System.Windows.DependencyObject CreateShell()
+        {
+            return ServiceLocator.Current.GetInstance<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow = (Window)Shell;
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
