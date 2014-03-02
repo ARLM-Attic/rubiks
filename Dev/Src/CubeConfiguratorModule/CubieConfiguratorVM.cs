badfc46e7527c0f5dd181c5828cbdde4c59246b5
SND\danielsandberg_cp <SND\danielsandberg_cp@1eac2b61-d0d7-4846-87ee-9410fe51cbb3>
2014-03-02 00:37:28 +0000

    Added buttons to manipulate the cube
    git-svn-id: https://rubiks.svn.codeplex.com/svn@32234 1eac2b61-d0d7-4846-87ee-9410fe51cbb3

--- Dev/Src/CubeConfiguratorModule/CubieConfiguratorVM.cs
+++ Dev/Src/CubeConfiguratorModule/CubieConfiguratorVM.cs
@@ -1,21 +1,38 @@
-﻿using RubiksCore;
+﻿using Microsoft.Practices.Prism.Commands;
+using RubiksCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace CubeConfiguratorModule
 {
     public class CubieConfiguratorVM
     {
+        ICommand _hotkeyCommand;
+
+        public CubieConfiguratorVM()
+        {
+            RubiksCube cube = new RubiksCube(null);
+            cube.Shuffle();
+            Cube = cube;
+
+            _hotkeyCommand = new CubeManipulationCommand(Cube);
+        }
+
         public RubiksCube Cube
+        {
+            get;
+            private set;
+        }
+
+        public ICommand CubeManipulationCommand
         {
             get
             {
-                RubiksCube cube = new RubiksCube(null);
-                cube.Shuffle();
-                return cube;
+                return _hotkeyCommand;
             }
         }
     }
