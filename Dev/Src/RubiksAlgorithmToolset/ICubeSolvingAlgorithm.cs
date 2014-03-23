b9d9943bcbd0368510236be5cb95dac93341ef9a
SND\danielsandberg_cp <SND\danielsandberg_cp@1eac2b61-d0d7-4846-87ee-9410fe51cbb3>
2014-03-23 21:37:32 +0000

    Created RubiksAlgorithmToolset
    git-svn-id: https://rubiks.svn.codeplex.com/svn@32489 1eac2b61-d0d7-4846-87ee-9410fe51cbb3

---
+++ Dev/Src/RubiksAlgorithmToolset/ICubeSolvingAlgorithm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubiksCore.RubiksAlgorithmToolset
+{
+    public interface ICubeSolvingAlgorithm
+    {
+        string AlgorithmName { get; }
+        string Author { get; }
+        string Description { get; }
+        void Solve(RubiksCube cube);
+    }
+}
