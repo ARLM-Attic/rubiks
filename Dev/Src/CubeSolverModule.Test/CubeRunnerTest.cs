c7f69505e94cdd3dd96103b42a79c27591b4df17
SND\danielsandberg_cp <SND\danielsandberg_cp@1eac2b61-d0d7-4846-87ee-9410fe51cbb3>
2014-03-23 23:47:59 +0000

    Created a unit test project for CubeSolverModule
    git-svn-id: https://rubiks.svn.codeplex.com/svn@32491 1eac2b61-d0d7-4846-87ee-9410fe51cbb3

---
+++ Dev/Src/CubeSolverModule.Test/CubeRunnerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CubeSolverModule.Test
+{
+    [TestClass]
+    public class CubeRunnerTest
+    {
+        [TestMethod]
+        public void Construct_ThenRunnerStateIsStopped()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void Run_WhenAlgorithmRunsToCompletion_ThenCubeStateGoesToRunningAndThenStopped()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void Run_WhenAlgorithmThrowsException_ThenCubeStateGoesToRunningAndThenErrorStateAndRunReturnsFalse()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void Run_WhenAlgorithmSolvesCube_ThenRunReturnsTrue()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void Run_WhenAlgorithmFailsToSolveCube_ThenRunReturnsFalse()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
