881e2336514ce2c41dd78132b4b05edb82520102
SND\danielsandberg_cp <SND\danielsandberg_cp@1eac2b61-d0d7-4846-87ee-9410fe51cbb3>
2014-03-29 03:48:11 +0000

    Created solver view and registered it with view registry
    git-svn-id: https://rubiks.svn.codeplex.com/svn@32540 1eac2b61-d0d7-4846-87ee-9410fe51cbb3

--- Dev/Src/CubeSolverModule/CubeSolverModule.cs
+++ Dev/Src/CubeSolverModule/CubeSolverModule.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CubeSolverModule.View;
 
 namespace CubeSolverModule
 {
@@ -11,10 +13,15 @@ namespace CubeSolverModule
     [ModuleDependency("CubeConfiguratorModule")]
     public class CubeSolverModule : IModule
     {
-        
+        IRegionViewRegistry _viewRegistry;
+
+        public CubeSolverModule(IRegionViewRegistry viewRegistry)
+        {
+            _viewRegistry = viewRegistry;
+        }
         public void Initialize()
         {
-            //throw new NotImplementedException();
+            _viewRegistry.RegisterViewWithRegion("solver", typeof(CubeRunnerView));
         }
     }
 }
